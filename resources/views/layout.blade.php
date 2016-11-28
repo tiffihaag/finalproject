de5f9e230603de83b067f2a4d551cda66c3660b7
tiffihaag <tiffihaag@gmail.com>
2016-11-27 19:33:10 -0500

    added register form

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,9 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <!--CSS-->
-    <link type="text/css" rel="stylesheet" href="public/css/style.css">
-
     <!--Materialize-->
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -26,53 +26,94 @@
     </form>
   </div>
 
-  <div class="register">
-    <!-- Modal content -->
-    <div class="modal-content" style="height: 500px;">
-      <span class="close">x</span>
+  <br>
+  <div class="center">
+    <button id="registerBtn">New to Totally Helpdesk? Register Here.</button>
+  </div>
+  <br><br>
+
+  <!-- The Modal -->
+  <div id="registerModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content" style="height: 750px;">
+    <span class="close">x</span>
       <form>
-        <div class="input-field col s9">
-          <div class="input-field col s3">
+       
+        <br>
+          <div class="input-field col s9">
             <input id="first" type="text" class="validate">
             <label for="first" data-error="wrong" data-success="right">First Name</label>
           </div>
 
-          <div class="input-field col s3">
+          <div class="input-field col s9">
             <input id="last" type="text" class="validate">
             <label for="last" data-error="wrong" data-success="right">Last Name</label>
           </div>
-        </div>
-
-        <div class="input-field col s9">
-          <div class="input-field col s3">
+       
+          <div class="input-field col s9">
             <input id="companyId" type="text" class="validate">
             <label for="companyId" data-error="wrong" data-success="right">Company Id</label>
           </div>
 
-          <div class="input-field col s3">
+          <div class="input-field col s9">
             <input id="company" type="text" class="validate">
             <label for="company" data-error="wrong" data-success="right">Company Name</label>
           </div>
-        </div>
-
-        <div class="input-field col s9">
-          <div class="input-field col s3">
+         
+          <div class="input-field col s9">
             <input id="address" type="text" class="validate">
             <label for="address" data-error="wrong" data-success="right">Address</label>
           </div>
 
-          <div class="input-field col s3">
-            <input id="first" type="text" class="validate">
-            <label for="first" data-error="wrong" data-success="right">First Name</label>
+          <div class="input-field col s9">
+            <input id="city" type="text" class="validate">
+            <label for="city" data-error="wrong" data-success="right">City</label>
           </div>
 
-          <div class="input-field col s3">
-            <input id="first" type="text" class="validate">
-            <label for="first" data-error="wrong" data-success="right">First Name</label>
+          <div class="input-field col s9">
+            <input id="state" type="text" class="validate">
+            <label for="state" data-error="wrong" data-success="right">State</label>
           </div>
 
-
-
+          <div class="input-field col s9">
+            <input id="zip" type="text" class="validate">
+            <label for="zip" data-error="wrong" data-success="right">Zip Code</label>
+          </div>
+        
+        <input type="submit" value="Submit" class="registerButton">
+    </form>
+    </div>
 
   </div>
+
+  <script>
+    // Get the modal
+    var registerModal = document.getElementById('registerModal');
+
+    // Get the button that opens the modal
+    var registerBtn = document.getElementById("registerBtn");
+
+    // Get the <span> element that closes the modal
+    var registerSpan = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    registerBtn.onclick = function() {
+      console.log("me");
+        registerModal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    registerSpan.onclick = function() {
+        registerModal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == registerModal) {
+            registerModal.style.display = "none";
+        }
+    }
+  </script>
+
 @endsection
